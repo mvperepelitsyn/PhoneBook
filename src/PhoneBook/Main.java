f38fbffa6acca9b37041f9616c6f06ddd2bfa40a
mvperepelitsyn <mvperepelitsyn@gmail.com>
2020-01-24 19:33:26 +0300

    It takes too much time

--- src/PhoneBook/Main.java
+++ src/PhoneBook/Main.java
@@ -6,14 +6,55 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static String[] gimmeThatArray(File fileName, boolean dirOrFind) {
+        /*
+        dirOrFind = true - stands for directory.txt
+        dirOrFind = false - stands for find.txt
+         */
         String tmp = "";
-        String mainStr = "";
-        File readFromThisFile = new File("/Users/dfrost-a/Desktop/Phonebook/find.txt");
-        try (Scanner scan = new Scanner(readFromThisFile)) {
-            mainStr = scan.nextLine();
+        String str = "";
+        try (Scanner scan = new Scanner(fileName)) {
+            tmp = dirOrFind ? (scan.next() + "#" + scan.nextLine()).split("#")[1].trim() : scan.nextLine();
+            while (!tmp.equals("")) {
+                str = str + tmp;
+                try {
+                    tmp = dirOrFind ? "#" + (scan.next() + "#" + scan.nextLine()).split("#")[1].trim() : "#" + scan.nextLine();
+                } catch (Exception e) {
+                    tmp = "";
+                }
+            }
         } catch (FileNotFoundException e) {
             System.out.println("Error");
         }
+        return str.split("#");
+    }
+
+    public static void main(String[] args) {
+        String tmp = "";
+        int foundSuc = 0;
+        long time = 0, min = 0, sec = 0, milisec = 0;
+        String findStr = "";
+        String dirStr = "";
+        String[] dirArray;
+        String[] findArray;
+        File readFromThisFileFind = new File("/Users/dfrost-a/Desktop/Phonebook/find.txt");
+        File readFromThisFileDir = new File("/Users/dfrost-a/Desktop/Phonebook/directory_orig.txt");
+        findArray = gimmeThatArray(readFromThisFileFind, false);
+        dirArray = gimmeThatArray(readFromThisFileDir, true);
+        time = System.currentTimeMillis();
+        for (int i = 0; i < findArray.length; i++) {
+        	for (int j = 0; j < dirArray.length; j++) {
+        		if (findArray[i].equals(dirArray[j])) {
+        			foundSuc++;
+        			break ;
+		        }
+	        }
+        }
+        time = System.currentTimeMillis() - time;
+        min = time / 60000;
+        sec = (time - min * 60000) / 1000;
+        milisec = (time - min * 60000 - sec * 1000);
+        System.out.println("Found " + foundSuc + " / " + findArray.length + " entries. Time taken: " +
+                 min + " min. " + sec + " sec. " + milisec + " ms.");
     }
 }
