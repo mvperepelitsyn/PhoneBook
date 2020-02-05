ff7a047508580a9cfab4d62298100ff87b97c56f
mvperepelitsyn <mvperepelitsyn@gmail.com>
2020-02-05 13:33:56 +0300

    what was on school's mac

--- src/PhoneBook/Main.java
+++ src/PhoneBook/Main.java
@@ -13,12 +13,14 @@ public class Main {
          */
         String tmp = "";
         String str = "";
+        String[] strArray = new String[1];
         try (Scanner scan = new Scanner(fileName)) {
-            tmp = dirOrFind ? (scan.next() + "#" + scan.nextLine()).split("#")[1].trim() : scan.nextLine();
-            while (!tmp.equals("")) {
-                str = str + tmp;
+            strArray = new String[dirOrFind ? 1014130 : 500];
+            tmp = scan.nextLine();
+            for (int i = 0; i < strArray.length && !tmp.equals(""); i++) {
+                strArray[i] = tmp;
                 try {
-                    tmp = dirOrFind ? "#" + (scan.next() + "#" + scan.nextLine()).split("#")[1].trim() : "#" + scan.nextLine();
+                    tmp = scan.nextLine();
                 } catch (Exception e) {
                     tmp = "";
                 }
@@ -26,35 +28,54 @@ public class Main {
         } catch (FileNotFoundException e) {
             System.out.println("Error");
         }
-        return str.split("#");
+        return strArray;
+    }
+
+    public static String gimmeThatName(String dirStr) {
+        String[] nameArray = dirStr.split(" ");
+        String nameStr = "";
+        for (int i = 1; i < nameArray.length; i++) {
+            nameStr = nameStr + nameArray[i];
+            if (i + 1 < nameArray.length) {
+                nameStr = nameStr + " ";
+            }
+        }
+        return nameStr;
     }
 
     public static void main(String[] args) {
         String tmp = "";
         int foundSuc = 0;
         long time = 0, min = 0, sec = 0, milisec = 0;
+        long timeParse;
         String findStr = "";
         String dirStr = "";
         String[] dirArray;
         String[] findArray;
         File readFromThisFileFind = new File("/Users/dfrost-a/Desktop/Phonebook/find.txt");
         File readFromThisFileDir = new File("/Users/dfrost-a/Desktop/Phonebook/directory_orig.txt");
+//        timeParse = System.currentTimeMillis();
         findArray = gimmeThatArray(readFromThisFileFind, false);
         dirArray = gimmeThatArray(readFromThisFileDir, true);
+//        timeParse = System.currentTimeMillis() - timeParse;
+//        min = timeParse / 60000;
+//        sec = (timeParse - min * 60000) / 1000;
+//        milisec = (timeParse - min * 60000 - sec * 1000);
+//        System.out.println("Parsing took " + min + " min. " + sec + " sec. " + milisec + " ms.");
         time = System.currentTimeMillis();
         for (int i = 0; i < findArray.length; i++) {
-//            for (int j = 0; j < dirArray.length; j++) {
-//                if (findArray[i].equals(dirArray[j])) {
-//                    foundSuc++;
-//                    break ;
-//                }
-//            }
-        	for (int j = 0, l = dirArray.length - 1; j < dirArray.length && l >= j; j++, l--) {
-        		if (findArray[i].equals(dirArray[j]) || findArray[i].equals(dirArray[l])) {
-        			foundSuc++;
-        			break ;
-		        }
-	        }
+            for (int j = 0; j < dirArray.length; j++) {
+                if (findArray[i].equals(gimmeThatName(dirArray[j]))) {
+                    foundSuc++;
+                    break ;
+                }
+            }
+//        	for (int j = 0, l = dirArray.length - 1; j < dirArray.length && l >= j; j++, l--) {
+//        		if (findArray[i].equals(dirArray[j]) || findArray[i].equals(dirArray[l])) {
+//        			foundSuc++;
+//        			break ;
+//		        }
+//	        }
         }
         time = System.currentTimeMillis() - time;
         min = time / 60000;
